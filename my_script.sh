3da03bd4120a916103e1b222bab80e1edd91eb4c
lemonde-L <35504551+lemonde-L@users.noreply.github.com>
2018-11-01 13:10:51 -0700

    Update my_script.sh

--- my_script.sh
+++ my_script.sh
@@ -4,10 +4,10 @@ echo "hello ITC-136"
 
 echo "edited in github"
 
-echo "This is your current working directory:"
+echo "This is current working directory:"
 pwd
 
-echo "This is the version of linux you're running:"
+echo "This is the version of linux you are running:"
 uname -a
 
 echo "These are your logged in users:"
