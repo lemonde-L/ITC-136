498acb851c1fe3ae38e58704aff50645d596c79a
lemonde-L <35504551+lemonde-L@users.noreply.github.com>
2018-12-10 00:55:09 -0800

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "how much disk space has been used"
+df
+
+echo "how much memory is free"
+free
+
+echo "how many connections there are to the current machine"
+netstat -a
+
+echo "who is logged in"
+who
